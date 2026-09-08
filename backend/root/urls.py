fe26e22f4dd7e433e025eac3bb57aa4350358e34
Fazliddin <fazliddinsuxrobov@gmail.com>
2026-09-08 15:08:16 +0500

    .

--- backend/root/urls.py
+++ backend/root/urls.py
@@ -11,12 +11,14 @@ SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/v1/', include('users.urls')),
     path('api/v1/product', include('product.urls')),
     path('api/v1/order', include('order.urls')),
 
+    
+
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Optional UI:
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
